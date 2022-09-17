58d6feb0943b3aeeb62e774c302dd99fec4507a9
Jon <jonathan-kings@hotmail.com>
2022-09-17 12:09:09 +0200

    Refactor argument parsing into model factory

--- ModelFactory.h
+++ ModelFactory.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include "Arguments.h"
 #include "Model.h"
+#include "RunAndTumble.h"
 
 class ModelFactory {
     ModelFactory()=delete;
@@ -9,7 +11,16 @@ class ModelFactory {
     ModelFactory(ModelFactory&&)=delete;
 public:
     
-    static Model* build(std::string name, std::size_t particleCount, unsigned int seed) {
+    static Model* build(Arguments& args) {
+        
+        std::string name = args.read<std::string>("model");
+        std::size_t particleCount = args.read<int>("particles", 512);
+        unsigned int seed = args.read<int>("seed", 0);
+        
+        if (name.compare("run-and-tumble") == 0) {
+            return new RunAndTumble(particleCount, args.read<float>("flip-prob", 0.1f), seed);
+        }
+        
         std::printf("Invalid model name %s!\n", name.c_str());
         std::exit(1);
     }
--- main.cpp
+++ main.cpp
@@ -5,21 +5,15 @@
 int main (int argc, char** argv) {
     
     // Read console args
-    std::string modelName;
+    Model* model = nullptr;
     std::size_t iterations;
-    std::size_t particleCount;
-    unsigned int seed;
     {
         Arguments args(argc, argv);
-        modelName = args.read<std::string>("model");
+        model = ModelFactory::build(args);
         iterations = args.read<int>("iter", 1000);
-        particleCount = args.read<int>("particles", 512);
-        seed = args.read<int>("seed", 0);
     }
     
     std::printf("Starting...\n\n");
-    
-    Model* model = ModelFactory::build(modelName, particleCount, seed);
     
     // Run the selected model for the given number of timesteps
     for (std::size_t i = 0; i < iterations; ++i) {

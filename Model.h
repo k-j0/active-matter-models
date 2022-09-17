0865ae67d1848bfe592bd2d3d77057e558c3d9cc
Jon <jonathan-kings@hotmail.com>
2022-09-17 12:07:16 +0200

    Make random number generation thread-safe

--- Model.h
+++ Model.h
@@ -2,17 +2,21 @@
 
 #include <random>
 #include <functional>
+#include <thread>
 
 class Model {
     
+    unsigned int seed;
+    
 protected:
-    // random number generator between 0 and 1
-    decltype(std::bind(std::uniform_real_distribution<float>(0.0f, 1.0f), std::mt19937())) rand01;
+    // thread-safe random number generator
+    float rand01 () {
+        static thread_local std::mt19937 gen(seed + std::hash<std::thread::id>{}(std::this_thread::get_id()));
+        return std::uniform_real_distribution<float>(0.0f, 1.0f)(gen);
+    }
     
 public:
-    Model (unsigned int seed) :
-        rand01(std::bind(std::uniform_real_distribution<float>(0.0f, 1.0f), std::mt19937(seed)))
-    { }
+    Model (unsigned int seed) : seed(seed) { }
     
     virtual ~Model () { }
     virtual void update() = 0;
